52fef1619524785d712d3776b975202aae2e84c1
Stefano Paulo <stefanopaulo47@gmail.com>
2021-04-05 18:41:36 -0300

    criando projeto

---
+++ app/src/main/java/com/app/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package com.app.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
